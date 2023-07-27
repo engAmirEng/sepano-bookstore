766cd9a7c1b1ccc53325574fe74f00e7e60b0ef1
AmirW <eng.amir.bu@gmail.com>
2023-07-27 18:26:45 +0330

    fix invalid url references to api instead of rest

--- name_goes_here/users/tests/test_drf_urls.py
+++ name_goes_here/users/tests/test_drf_urls.py
@@ -4,15 +4,15 @@ from name_goes_here.users.models import User
 
 
 def test_user_detail(user: User):
-    assert reverse("api:user-detail", kwargs={"username": user.username}) == f"/api/users/{user.username}/"
-    assert resolve(f"/api/users/{user.username}/").view_name == "api:user-detail"
+    assert reverse("rest:user-detail", kwargs={"username": user.username}) == f"/api/users/{user.username}/"
+    assert resolve(f"/api/users/{user.username}/").view_name == "rest:user-detail"
 
 
 def test_user_list():
-    assert reverse("api:user-list") == "/api/users/"
-    assert resolve("/api/users/").view_name == "api:user-list"
+    assert reverse("rest:user-list") == "/api/users/"
+    assert resolve("/api/users/").view_name == "rest:user-list"
 
 
 def test_user_me():
-    assert reverse("api:user-me") == "/api/users/me/"
-    assert resolve("/api/users/me/").view_name == "api:user-me"
+    assert reverse("rest:user-me") == "/api/users/me/"
+    assert resolve("/api/users/me/").view_name == "rest:user-me"
